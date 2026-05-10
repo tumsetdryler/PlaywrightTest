0278988a0b21564ccf1ab8b418322deec196c437
TumsetDryler <tyler199900@gmail.com>
2026-05-09 21:56:35 -0700

    Add helper functions to reuse code, change fills to pressSequentially to mimic user input. Webkit browsers seem to have issues with the date function, so there is an exception for it.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"This repo is a self-study in Playwright, coming from someone who has used Selenium and Appium for automated testing." 
+This repo is a self-study in Playwright, coming from someone who has used Selenium and Appium for automated testing.
+
+Initial thoughts on the project: Playwright is very simple and feels basically the same as using Selenium, just with different syntax.
+
+GitHub Actions were easy to set up and seem to be a powerful CI/CD tool.
--- tests/first-test.spec.js
+++ tests/first-test.spec.js
@@ -1,5 +1,23 @@
 import { test, expect } from '@playwright/test';
 
+// Helper Functions
+
+async function verify_web_inputs_fields(page) {
+  // Verify all inputs are visible and empty
+  console.log("Helper started");
+  await expect(page.getByRole('spinbutton', { name: 'Input: Number' })).toBeVisible();
+  await expect(page.getByRole('textbox', { name: 'Input: Text' })).toBeVisible();
+  await expect(page.getByRole('textbox', { name: 'Input: Password' })).toBeVisible();
+  await expect(page.getByRole('textbox', { name: 'Input: Date' })).toBeVisible();
+  await expect(page.getByRole('spinbutton', { name: 'Input: Number' })).toBeEmpty();
+  await expect(page.getByRole('textbox', { name: 'Input: Text' })).toBeEmpty();
+  await expect(page.getByRole('textbox', { name: 'Input: Password' })).toBeEmpty();
+  await expect(page.getByRole('textbox', { name: 'Input: Date' })).toBeEmpty();
+  console.log("Helper finished");
+}
+
+// End Helper Functions
+
 test.beforeEach(async ({ page }) => {
   // Set up network interception to block ads
   await page.route(/(ads|doubleclick|googlesyndication|tracker)/i, route => {
@@ -15,29 +33,23 @@ test('landing page has title', async ({ page }) => {
   await expect(page).toHaveTitle("Automation Testing Practice Website for QA and Developers | UI and API");
 });
 
-test('web inputs page positive functionality', async ({ page }) => {
+test('web inputs page positive functionality', async ({ page, browserName }) => {
   // Verify link is available and click
   await expect(page.getByRole('link', { name: 'Web inputs' })).toBeVisible();
   await page.getByRole('link', { name: 'Web inputs' }).click();
 
-  // Verify all inputs are visible and empty
-  await expect(page.getByRole('spinbutton', { name: 'Input: Number' })).toBeVisible();
-  await expect(page.getByRole('textbox', { name: 'Input: Text' })).toBeVisible();
-  await expect(page.getByRole('textbox', { name: 'Input: Password' })).toBeVisible();
-  await expect(page.getByRole('textbox', { name: 'Input: Date' })).toBeVisible();
-  await expect(page.getByRole('spinbutton', { name: 'Input: Number' })).toBeEmpty();
-  await expect(page.getByRole('textbox', { name: 'Input: Text' })).toBeEmpty();
-  await expect(page.getByRole('textbox', { name: 'Input: Password' })).toBeEmpty();
-  await expect(page.getByRole('textbox', { name: 'Input: Date' })).toBeEmpty();
+  await verify_web_inputs_fields(page);
+  console.log("Out of helper");
 
   // Input values into each input field
   await page.getByRole('spinbutton', { name: 'Input: Number' }).click();
-  await page.getByRole('spinbutton', { name: 'Input: Number' }).fill('32');
+  await page.getByRole('spinbutton', { name: 'Input: Number' }).pressSequentially('32');
   await page.getByRole('textbox', { name: 'Input: Text' }).click();
-  await page.getByRole('textbox', { name: 'Input: Text' }).fill('bazinga');
+  await page.getByRole('textbox', { name: 'Input: Text' }).pressSequentially('bazinga');
   await page.getByRole('textbox', { name: 'Input: Password' }).click();
-  await page.getByRole('textbox', { name: 'Input: Password' }).fill('planet');
-  await page.getByRole('textbox', { name: 'Input: Date' }).fill('2010-01-16');
+  await page.getByRole('textbox', { name: 'Input: Password' }).pressSequentially('planet');
+  await page.getByRole('textbox', { name: 'Input: Date' }).click();
+  await page.getByRole('textbox', { name: 'Input: Date' }).pressSequentially('01162010');
 
   // Click Display Inputs button
   await page.getByRole('button', { name: 'Display Inputs' }).click();
@@ -46,18 +58,19 @@ test('web inputs page positive functionality', async ({ page }) => {
   await expect(page.locator('#output-number')).toContainText('32');
   await expect(page.locator('#output-text')).toContainText('bazinga');
   await expect(page.locator('#output-password')).toContainText('planet');
-  await expect(page.locator('#output-date')).toContainText('2010-01-16');
+  if(browserName === 'webkit')
+  {
+    await expect(page.locator('#output-date')).toContainText('01162010');
+  }
+  else
+  {
+    await expect(page.locator('#output-date')).toContainText('2010-01-16');
+  }
+  
+  
 
   // Clear the page with the Clear Inputs button
   await page.getByRole('button', { name: 'Clear Inputs' }).click();
 
-  // Verify all inputs are visible and empty
-  await expect(page.getByRole('spinbutton', { name: 'Input: Number' })).toBeVisible();
-  await expect(page.getByRole('textbox', { name: 'Input: Text' })).toBeVisible();
-  await expect(page.getByRole('textbox', { name: 'Input: Password' })).toBeVisible();
-  await expect(page.getByRole('textbox', { name: 'Input: Date' })).toBeVisible();
-  await expect(page.getByRole('spinbutton', { name: 'Input: Number' })).toBeEmpty();
-  await expect(page.getByRole('textbox', { name: 'Input: Text' })).toBeEmpty();
-  await expect(page.getByRole('textbox', { name: 'Input: Password' })).toBeEmpty();
-  await expect(page.getByRole('textbox', { name: 'Input: Date' })).toBeEmpty();
+  await verify_web_inputs_fields(page);
 });
